876d89955771f7020f848d697094f12abde053e2
Zetaniis <63559030+Zetaniis@users.noreply.github.com>
2025-08-03 16:51:43 +0200

    feat: general mark settings set

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {Mark} from 'tether-marks-core';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,13 +11,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tether-marks-for-vscode" is now active!');
 
+	const getMarks = () => {return context.globalState.get("marks") as Mark[] | undefined};
+	const setMarks = (marks: Mark[]) => {return context.globalState.update("marks", marks)};
+
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('tether-marks-for-vscode.set-mark', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tether-marks-for-vscode!');
+
 	});
 	const disposable2 = vscode.commands.registerCommand('tether-marks-for-vscode.go-to-mark', () => {
 		// The code you place here will be executed every time your command is executed
